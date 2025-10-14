97652cdb25a084a01ee31f7dfb6c2dd97eb6e3ca
ABEEGOLD <abeeutee@gmail.com>
2025-10-14 01:07:29 +0100

    first commit on creating the project

---
+++ ArtSalesSystem/src/main/java/com/semicolon/africa/artsalessystem/ArtSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.artsalessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSalesSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSalesSystemApplication.class, args);
+    }
+
+}
---
+++ ArtSalesSystem/src/test/java/com/semicolon/africa/artsalessystem/ArtSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.artsalessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSalesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
